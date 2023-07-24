a0cb1da89704d568520160cad62847ee1464b2b8
Ian Ovenden <ian.ovenden@gmail.com>
2023-07-24 17:07:08 +0100

    Updated Mimic A Sentence functionality

--- src/components/HMRC_ODX_MimicASentence/index.jsx
+++ src/components/HMRC_ODX_MimicASentence/index.jsx
@@ -1,35 +1,49 @@
-import { useContext } from 'react';
-import { Grid , Flex } from '@pega/cosmos-react-core';
+import { useContext, useEffect, useState, Fragment } from 'react';
+import { Grid, Flex } from '@pega/cosmos-react-core';
 import PropTypes from 'prop-types';
 
 import StyledHmrcOdxMimicASentenceWrapper from './styles';
-
 
 // Duplicated runtime code from Constellation Design System Component
 
 // props passed in combination of props from property panel (config.json) and run time props from Constellation
 // any default values in config.pros should be set in defaultProps at bottom of this file
 export default function HmrcOdxMimicASentence(props) {
-
-
   const { getPConnect, children, template, label, NumCols } = props;
 
   let nCols = parseInt(NumCols);
+  const [formElms, setFormElms] = useState([]);
 
   //console.log(`Rendering ${getPConnect()?.getComponentName()} with ${template} with ${children?.length} Region(s)`);
 
+  useEffect(() => {
+    const elms = [];
+    const region = children[0] ? children[0].props.getPConnect() : null;
+    if (region?.getChildren()) {
+      region.getChildren().map(child => {
+        child.getPConnect().setInheritedProp('readOnly', true);
+        elms.push(child.getPConnect().getComponent());
+      });
+      setFormElms(elms);
+    }
+  }, [children[0]]);
+
   return (
     <StyledHmrcOdxMimicASentenceWrapper>
-    <Grid container={{
-          cols: `repeat(${nCols}, minmax(0, 1fr))`,
-          gap: 2,
-          wrap: `wrap`
-        }}>
-          {children}
-    </Grid>
+      <Flex>
+        {formElms.map((field, index) => {
+          const formattedValue = field.props.DateTimeFormat
+            ? new Date(field.props.value).toLocaleDateString('en-GB', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric'
+              })
+            : field.props.value;
+          return <Fragment key={index}>{formattedValue}&nbsp;</Fragment>;
+        })}
+      </Flex>
     </StyledHmrcOdxMimicASentenceWrapper>
   );
-
 }
 
 HmrcOdxMimicASentence.defaultProps = {
